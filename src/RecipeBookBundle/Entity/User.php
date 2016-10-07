d6f38a2daf1df264ad192df9cd47498f155a41ad
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-10-07 10:11:14 +0200

    Add relation to Recipe.

--- src/RecipeBookBundle/Entity/User.php
+++ src/RecipeBookBundle/Entity/User.php
@@ -18,8 +18,47 @@ class User extends BaseUser {
      */
     protected $id;
 
+    /**
+     * @ORM\OneToMany(targetEntity="Recipe", mappedBy="user")
+     */
+    private $recipes;
+
     public function __construct() {
         parent::__construct();
+        $this->recipes = new ArrayCollection();
     }
 
+
+    /**
+     * Add recipes
+     *
+     * @param \RecipeBookBundle\Entity\Recipe $recipes
+     * @return User
+     */
+    public function addRecipe(\RecipeBookBundle\Entity\Recipe $recipes)
+    {
+        $this->recipes[] = $recipes;
+
+        return $this;
+    }
+
+    /**
+     * Remove recipes
+     *
+     * @param \RecipeBookBundle\Entity\Recipe $recipes
+     */
+    public function removeRecipe(\RecipeBookBundle\Entity\Recipe $recipes)
+    {
+        $this->recipes->removeElement($recipes);
+    }
+
+    /**
+     * Get recipes
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getRecipes()
+    {
+        return $this->recipes;
+    }
 }
