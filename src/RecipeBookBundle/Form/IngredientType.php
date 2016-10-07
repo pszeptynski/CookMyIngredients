2958f510895b10b51b372f2578879b6864cd7c48
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-10-07 11:02:01 +0200

    Add crud generated form. Remove recipe from form.

---
+++ src/RecipeBookBundle/Form/IngredientType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace RecipeBookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class IngredientType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('quantity')
+//            ->add('recipe')
+            ->add('dictionary')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RecipeBookBundle\Entity\Ingredient'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'recipebookbundle_ingredient';
+    }
+}
