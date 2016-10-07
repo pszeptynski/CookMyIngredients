0029e1511a096b6466be30166f4f926f1feb4b40
Piotr Szeptynski <p.szeptynski@gmail.com>
2016-10-07 14:36:00 +0200

    Add simple db query to get recipe.

--- src/RecipeBookBundle/Controller/DefaultController.php
+++ src/RecipeBookBundle/Controller/DefaultController.php
@@ -6,14 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
-     * @Route("/hello/{name}")
+     * @Route("/", name="homepage")
      * @Template()
      */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
+    public function indexAction($name) {
+//        return array('name' => $name);
+        // pobieranie zalogowanego usera
+//        $user = new User();
+//        $loggedUser = $user->getUser(); //getUser wbudowane w Sf
+//        $content = $this->renderView('AcmeReadBundle:Show:index.html.twig',$product);
+//    return new Response($content);
+
+        $em = $this->getDoctrine()->getManager();
+        $id = 3;
+        $entity = $em->getRepository('RecipeBookBundle:Recipe')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Recipe entity.');
+        }
+        return array(
+            'entity' => $entity,
+        );
     }
+
 }
